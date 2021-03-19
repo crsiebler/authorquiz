323cc923f6499412dd64617de19caad54a1b3e49
Cory Siebler <cory.siebler@protonmail.com>
2021-03-19 12:44:19 -0700

    Updates

---
+++ precache-manifest.6905d1fc8241f3ad8da92d755abdc1c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0699d4eb4d36c62cf36648692eb6bf3e",
+    "url": "/authorquiz/index.html"
+  },
+  {
+    "revision": "4b9978e99d0b511ba8cc",
+    "url": "/authorquiz/static/css/main.46b249b8.chunk.css"
+  },
+  {
+    "revision": "344d4f9604e5f9a21c5c",
+    "url": "/authorquiz/static/js/2.566cb6fe.chunk.js"
+  },
+  {
+    "revision": "4b9978e99d0b511ba8cc",
+    "url": "/authorquiz/static/js/main.5320a79b.chunk.js"
+  },
+  {
+    "revision": "57cd4876dffa607a8b7f",
+    "url": "/authorquiz/static/js/runtime~main.9ad94e50.js"
+  }
+]);
